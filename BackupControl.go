a457b75824d4b0dcc49b26010b33cb1a10b9d289
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2019-05-24 17:03:27 +0300

    code switched to use my dblist package

--- BackupControl.go
+++ BackupControl.go
@@ -2,7 +2,9 @@
 package main // import "BackupsControl"
 
 import (
-	"BackupsControl/dpapi" 
+	"BackupsControl/dblist/v2"
+	"BackupsControl/dpapi"
+	"BackupsControl/sendmail"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,11 +12,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"BackupsControl/sendmail"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
+
 	"github.com/pkg/profile"
 )
 
@@ -22,6 +23,7 @@ type pathandfilenames struct {
 	Path        string
 	Filename    string
 	Days        int
+	Group       string
 	modtime     time.Time
 	hasAnyFiles bool // indicating there were some files to choose from
 }
@@ -29,27 +31,33 @@ type email struct {
 	Email []byte
 }
 
-func readConfig(filename string) []pathandfilenames {
-	f, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-	b, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-	if b[0] == 0xEF || b[0] == 0xBB || b[1] == 0xBB {
-		b = b[3:]
-	}
+// func readConfig(filename string) []pathandfilenames {
+// 	f, err := os.Open(filename)
+// 	if err != nil {
+// 		log.Fatalf("%s", err)
+// 	}
+// 	b, err := ioutil.ReadAll(f)
+// 	if err != nil {
+// 		log.Fatalf("%s", err)
+// 	}
+// 	if b[0] == 0xEF || b[0] == 0xBB || b[1] == 0xBB { // has BOM?
+// 		b = b[3:]
+// 	}
 
-	var datastruct []pathandfilenames
-	err = json.Unmarshal(b, &datastruct)
-	if err != nil {
-		log.Fatalf("Zaerror: json structure bad.\n%s", err)
-	}
-	return datastruct
-}
+// 	var datastruct []pathandfilenames
+// 	err = json.Unmarshal(b, &datastruct)
+// 	if err != nil {
+// 		log.Fatalf(`error: json structure is bad.\n\
+// 		%s\n\
+// 		Expects:[
+// 			{"path":"j:/b", "Filename":"A2", "group":"-FULL", "Days":7},
+// 			...
+// 			]`, err)
+// 	}
+// 	return datastruct
+// }
 
+// DecryptEmail returns password decrypted by Microsoft DPAPI
 func DecryptEmail(b []byte) string {
 	var emailobj email
 	err := json.Unmarshal(b, &emailobj)
@@ -64,150 +72,44 @@ func DecryptEmail(b []byte) string {
 	return s
 }
 
-func extractGroupName(s string) string {
-	// all filenames are in the form dbnamehere_2018-10-08T08-00-00-497-FULL.bak
-	// if strings.HasPrefix(s, "ubcd_sklad_2010") {
-	// 	fmt.Print(s)
-	// }
-	year := time.Now().Year()
-	pos := strings.Index(s, "_"+strconv.Itoa(year))
-	if pos == -1 {
-		return ""
-	}
-	return string(s[:pos])
-}
-func findLastWithGroupName(m []pathandfilenames) {
+// func extractGroupName(s string) string {
+// 	// all filenames are in the form dbnamehere_2018-10-08T08-00-00-497-FULL.bak
+// 	// if strings.HasPrefix(s, "ubcd_sklad_2010") {
+// 	// 	fmt.Print(s)
+// 	// }
+// 	year := time.Now().Year()
+// 	pos := strings.Index(s, "_"+strconv.Itoa(year))
+// 	if pos == -1 {
+// 		return ""
+// 	}
+// 	return string(s[:pos])
+// }
+// func findLastWithGroupName(m []pathandfilenames) {
 
-}
-func getUniquePaths(configstruct []pathandfilenames) map[string]int {
-	retmap := make(map[string]int)
-	for _, str := range configstruct {
-		if _, ok := retmap[str.Path]; !ok {
-			retmap[str.Path] = 1
-		}
-	}
-	return retmap
-}
-func readFilesFrompaths(uniqueconfigpaths map[string]int) map[string][]os.FileInfo {
+// }
+// func getUniquePaths(configstruct []pathandfilenames) map[string]int {
+// 	retmap := make(map[string]int)
+// 	for _, str := range configstruct {
+// 		if _, ok := retmap[str.Path]; !ok {
+// 			retmap[str.Path] = 1
+// 		}
+// 	}
+// 	return retmap
+// }
+// func readFilesFrompaths(uniqueconfigpaths map[string]int) map[string][]os.FileInfo {
 
-	retmap := make(map[string][]os.FileInfo)
-	for k := range uniqueconfigpaths {
-		filesinfo, err := ioutil.ReadDir(k)
-		if err != nil {
-			log.Fatalf("%s", err)
-		}
+// 	retmap := make(map[string][]os.FileInfo)
+// 	for k := range uniqueconfigpaths {
+// 		filesinfo, err := ioutil.ReadDir(k)
+// 		if err != nil {
+// 			log.Fatalf("%s", err)
+// 		}
 
-		retmap[k] = filesinfo
+// 		retmap[k] = filesinfo
 
-	}
-	return retmap // map of slices of fileinfos
-}
-
-// FindMissedBackups seeks for last file in every group
-// looks like MAXIMUM(Filename) GROUP BY groupName
-func FindMissedBackups(existingFiles map[string][]os.FileInfo,
-	configstruct []pathandfilenames, uniqueconfigpaths map[string]int) []string {
-
-	lenconfig := len(configstruct)
-	ret := make([]string, 0, 20)
-	for k := range existingFiles {
-		sort.Slice(existingFiles[k], func(i, j int) bool {
-			return existingFiles[k][i].Name() < existingFiles[k][i].Name()
-		})
-
-		// searching the last filename in the group
-		var prevGroupName string
-		for _, val := range existingFiles[k] {
-
-			prevGroupName = extractGroupName(val.Name())
-			if prevGroupName != "" {
-
-				break
-			}
-		}
-		var lastFiles []pathandfilenames // last by time files in the group
-		for i, stPathAndFilenames := range existingFiles[k] {
-			groupName := extractGroupName(stPathAndFilenames.Name())
-			if groupName != prevGroupName { // start of next group groupName == FoodTechnologiesGrishinaCopy
-				if prevGroupName == "" {
-					prevGroupName = groupName // current group ended
-					//fmt.Fprintf(os.Stderr, "Not possible to extract group name: %s\n", stPathAndFilenames.Name())
-					continue
-
-				}
-
-				if existingFiles[k][i-1].IsDir() {
-					prevGroupName = groupName // current group ended
-					continue
-				}
-				prevGroupName = groupName // current group ended
-				// adding _previous_ Filename
-				lastFiles = append(lastFiles,
-					pathandfilenames{
-						Path:     k,
-						Filename: existingFiles[k][i-1].Name(),
-						modtime:  existingFiles[k][i-1].ModTime(),
-					})
-
-			}
-		}
-		//the last element may trugger appending to lastFiles
-		dl := len(existingFiles[k])
-		groupName := extractGroupName(existingFiles[k][dl-1].Name())
-		if !existingFiles[k][dl-1].IsDir() && groupName != "" {
-			lastFiles = append(lastFiles,
-				pathandfilenames{
-					Path:     k,
-					Filename: existingFiles[k][dl-1].Name(),
-					modtime:  existingFiles[k][dl-1].ModTime(),
-				})
-		}
-
-		// For every file from lastFiles checks difference with current datetime
-		curTime := time.Now()
-		minTimeOfFile := time.Date(curTime.Year(),
-			curTime.Month(), curTime.Day(), 0, 0, 0, 0, curTime.Location())
-		if minTimeOfFile.Weekday() == time.Monday {
-			minTimeOfFile.AddDate(0, 0, -3)
-		}
-		for _, pathandfile := range lastFiles {
-			groupName := extractGroupName(pathandfile.Filename)
-			if groupName == "" {
-				continue
-			}
-			configline := sort.Search(lenconfig, func(n int) bool {
-
-				return configstruct[n].Filename >= groupName
-			})
-			if configline == lenconfig ||
-				configstruct[configline].Filename != groupName {
-				continue // actual file is not in the configstruct
-			}
-			days := configstruct[configline].Days
-			if days == 0 {
-				days = 1
-			}
-
-			configstruct[configline].hasAnyFiles = true
-
-			timepoint := minTimeOfFile.Add(-24 * time.Duration(days) * time.Hour)
-
-			if timepoint.Sub(pathandfile.modtime) > 0 {
-				//fmt.Printf("%s vs. %s \t %s\n", pathandfile.modtime, timepoint, pathandfile.Filename)
-				ret = append(ret, pathandfile.Filename+"\t\t in "+pathandfile.Path)
-			}
-		}
-
-	}
-	// if there were no files for backup group
-	for _, confstr := range configstruct {
-		if !confstr.hasAnyFiles {
-			//fmt.Printf("%s \t\t in %s\n", confstr.Filename, confstr.Path)
-			ret = append(ret, confstr.Filename+" \t\t  in "+confstr.Path)
-		}
-	}
-	return ret
-}
+// 	}
+// 	return retmap // map of slices of fileinfos
+// }
 
 func savePasswordToFile(configfilename *string, savepassword *string) {
 	f, err := os.OpenFile(*configfilename+"_email", os.O_CREATE|os.O_TRUNC, 0)
@@ -232,8 +134,9 @@ func printUsage(w io.Writer) {
 	fmt.Fprintf(w, "%s", "Usage: BackupsControl -configfilename <name> [-savepassword password]\n")
 	return
 }
+
 func main() {
-	defer profile.Start(profile.MemProfile,profile.ProfilePath(".")).Stop()
+	defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
 	configfilename := flag.String("configfilename", "", `json config file name. Content is [{"path":"j:\b", "Filename":"base1", "Days":2}, ...]`)
 	savepassword := flag.String("savepassword", "", "Saves your email password using DPAPI in your config file.")
 	flag.Parse()
@@ -251,17 +154,76 @@ func main() {
 		savePasswordToFile(configfilename, savepassword)
 		return
 	}
-	configstruct := readConfig(*configfilename)
-	sort.Slice(configstruct, func(i, j int) bool { // to have binary search
-		return configstruct[i].Filename < configstruct[j].Filename
+
+	ConfigItems, err := dblist.ReadConfig(*configfilename)
+	if err != nil {
+		log.Fatalf("%s\n%s\n", "Error: can't read config file.", err)
+	}
+	// config lines sorted ascending by filename and suffix
+	sort.Slice(ConfigItems, func(i, j int) bool {
+		// sorts ascending
+		if ConfigItems[i].Filename < ConfigItems[j].Filename {
+			return true
+		}
+		if ConfigItems[i].Filename == ConfigItems[j].Filename &&
+			ConfigItems[i].Suffix < ConfigItems[j].Suffix {
+			return true // ascending order
+		}
+		return false
 	})
-	uniqueconfigpaths := getUniquePaths(configstruct)        // from config file gets unique folders
-	actualFilesInfo := readFilesFrompaths(uniqueconfigpaths) // for every folder gets []filesinfo, retuns map[folder]
 
-	absentBackups := FindMissedBackups(actualFilesInfo, configstruct, uniqueconfigpaths)
+	uniqueconfigpaths := dblist.GetUniquePaths(ConfigItems)         // from config file gets unique folders
+	actualFilesInfo := dblist.ReadFilesFromPaths(uniqueconfigpaths) // for every folder gets []filesinfo, retuns map[folder]
 
-	if len(absentBackups) > 0 {
-		// mail
+	currentSuffixes := []string{"-FULL.bak", "-differ.dif", "-FULL.rar", "-differ.rar", ".rar", ".7z"}
+
+	type outdatedBackup struct {
+		dblist.FileInfoWin
+		pLine dblist.ConfigLine
+	}
+	var outdatedFiles []outdatedBackup
+
+	for k := range actualFilesInfo {
+
+		lastFiles := dblist.GetLastFilesGroupedByFunc(actualFilesInfo[k],
+			dblist.GroupFunc,
+			//bigger(more wide) suffixes comes first
+			currentSuffixes)
+
+		// next decides wether file is outdated
+		for _, v := range lastFiles {
+			line := dblist.FindConfigLineByFilename(v.Name(), currentSuffixes, ConfigItems)
+			if line == nil {
+				fmt.Printf("Error: for filename %s there is no line in config file.\n", v.Name())
+				continue
+			}
+			days := line.Days
+			if days == 0 {
+				days = 1
+			}
+			if time.Now().Hour() > 14 {
+				days++
+			}
+			if !line.HasAnyFiles {
+				line.HasAnyFiles = true // mark config line that there are some files
+			}
+			howold := time.Now().Sub(v.ModTime())
+			if howold > time.Hour*time.Duration(days*24) {
+				aFile := outdatedBackup{v, *line}
+				outdatedFiles = append(outdatedFiles, aFile)
+			}
+		}
+	}
+	// check if there are any files for evere config line
+	var noFilesAtAll []dblist.ConfigLine
+	for _, v := range ConfigItems {
+		if !v.HasAnyFiles {
+			noFilesAtAll = append(noFilesAtAll, v)
+		}
+	}
+
+	if len(outdatedFiles) > 0 || len(noFilesAtAll) > 0 {
+		// send an email
 		newconn, _, err := sendmail.GetTLSConnection()
 		defer newconn.Close()
 		if err != nil {
@@ -277,8 +239,19 @@ func main() {
 		}
 		password := DecryptEmail(b)
 		c := sendmail.Authenticate(newconn, password)
-
-		body := strings.Join(absentBackups, "\n")
+		var sb strings.Builder
+		sb.WriteString("------ Last outdated backups: -------\n")
+		for _, v := range outdatedFiles {
+			sb.WriteString("every ")
+			sb.WriteString(fmt.Sprintf("%d", v.pLine.Days))
+			sb.WriteString(" days 		")
+			sb.WriteString(v.Name())
+			sb.WriteString("\n")
+		}
+		for _, v := range noFilesAtAll {
+			sb.WriteString(fmt.Sprintf(`no backups for config line: {"path":"%s", "filename":"%s", "suffix":"%s"}`+"\n", v.Path, v.Filename, v.Suffix))
+		}
+		body := sb.String()
 		if true {
 			fmt.Print(body)
 			sendmail.SendMailToMe(c, "arch3", body, "arch3")
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module BackupsControl
 
 require BackupsControl/dpapi v0.0.1
 
+require BackupsControl/dblist/v2 v2.0.0
+
 require (
 	github.com/pkg/profile v1.3.0
 	github.com/rsc/benchgraffiti v0.0.0-20170203011114-ff448abfc41f // indirect
@@ -9,3 +11,5 @@ require (
 )
 
 replace BackupsControl/dpapi v0.0.1 => ../dpapi
+
+replace BackupsControl/dblist/v2 v2.0.0 => ../../GO/src/dblist/v2
